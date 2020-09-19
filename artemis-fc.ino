0d762c66248e4e47ec9188e5206ccd1973f67203
planeflyer <cam201516phc@gmail.com>
2020-09-19 16:23:01 +0900

    Implemented auto controls (incl. the echo sensor.)

--- artemis-fc.ino
+++ artemis-fc.ino
@@ -8,28 +8,29 @@ Servo motor;
 Servo rudder;
 Servo elevator;
 
-float P_yaw = 1;
-float I_yaw = 0;
-float D_yaw = 0;
-float P_pitch = -1;
-float I_pitch = 0;
-float D_pitch = 0;
-
+// variables for PID control
 int roll_sum = 0; //overflowに注意
 int roll_before = 0;
 int pitch_sum = 0;
 int pitch_before = 0;
 
+// variables for manual control filters
 int rtemp1 = 0;
 int rtemp2 = 0;
-
 int etemp1 = 0;
 int etemp2 = 0;
 
-int cnt = 0;
-
+// variables for 9-axis sensor calibration
 float pitchZero = 0;
 float rollZero = 0;
+
+// constants for auto flight
+float eight_start_yaw = 0;
+float climb_start_yaw = 0;
+float climb_start_alt = 0;
+enum State {BEFORE_START, FIRST_TURN, CLIMB, SECOND_TURN, DONE };
+enum State eight_state = BEFORE_START;
+enum State climb_state = BEFORE_START;
 
 void setup() {
   // put your setup code here, to run once:
@@ -58,6 +59,9 @@ void setup() {
   pitchZero = pitchSum /repetition;
   rollZero = rollSum / repetition;
 
+  pinMode(11, OUTPUT); //trigger_pin
+  pinMode(7, INPUT); //echo_pin
+
   digitalWrite(PIN_LED2,LOW);
 }
 
@@ -73,23 +77,72 @@ void loop() {
     d = sbus.read();
   }
   if(dat[0] == 240){
-    if(dat[9] & 0x02 > 0){
+    if(dat[9] & 0x02 > 0){ //自動操縦モード
       mpu.update();
-      // センサーデータを取る
       digitalWrite(PIN_LED1,HIGH);
-      int target_roll = 0;
-      int target_pitch = 0;
-      if(dat[10] & 0x10 > 0) target_roll = 15;
-      else if(dat[11] & 0x80 > 0) target_roll = -15;
-      if(dat[14]&0x20 > 0) target_pitch = 10;
-
-      int tau_roll = PIDcontrol(mpu.getRoll(),rollZero,target_roll,&roll_before,P_yaw,I_yaw,D_yaw);
-      int tau_pitch = PIDcontrol(mpu.getPitch(),pitchZero,target_pitch,&pitch_before,P_pitch,I_pitch,D_pitch);
+      int tau_roll;
+      int tau_pitch;
+      if(dat[10] & 0x10 > 0){ //CH7（水平旋回）
+        tau_roll = PIDcontrol(mpu.getRoll(),rollZero,15,&roll_before,1,0,0);
+        tau_pitch = PIDcontrol(mpu.getPitch(),pitchZero,0,&pitch_before,-1,0,0);
+      }
+      else if(dat[11] & 0x80 > 0) { //CH8（8の字飛行）
+        switch (eight_state)
+        {
+        case BEFORE_START:
+          eight_state = FIRST_TURN;
+          eight_start_yaw = mpu.getYaw();
+          break;
+        case FIRST_TURN:
+          tau_roll = PIDcontrol(mpu.getRoll(),rollZero,15,&roll_before,1,0,0);
+          tau_pitch = PIDcontrol(mpu.getPitch(),pitchZero,0,&pitch_before,-1,0,0);
+          float remain = mpu.getYaw() - eight_start_yaw;
+          if(-5 < remain && remain < 5) eight_state = SECOND_TURN;
+          break;
+        case SECOND_TURN:
+          tau_roll = PIDcontrol(mpu.getRoll(),rollZero,-15,&roll_before,1,0,0);
+          tau_pitch = PIDcontrol(mpu.getPitch(),pitchZero,0,&pitch_before,-1,0,0);
+          break;
+        default:
+          break;
+        }
+      }
+      else if(dat[13]&0x04 > 0){ //CH9（自動離陸）
+        motor.write(180);
+        tau_roll = PIDcontrol(mpu.getRoll(),rollZero,0,&roll_before,1,0,0);
+        tau_pitch = PIDcontrol(mpu.getPitch(),pitchZero,10,&pitch_before,-1,0,0);
+      }
+      else if(dat[14]&0x20 > 0){ //CH10（上昇旋回）
+        tau_roll = PIDcontrol(mpu.getRoll(),rollZero,-15,&roll_before,1,0,0);
+        switch (climb_state)
+        {
+        case BEFORE_START:
+          climb_state = FIRST_TURN;
+          climb_start_yaw = mpu.getYaw();
+          climb_start_alt = echo();
+          break;
+        case FIRST_TURN:
+          tau_pitch = PIDcontrol(mpu.getPitch(),pitchZero,0,&pitch_before,-1,0,0);
+          float remain = mpu.getYaw() - start_yaw;
+          if(-5 < remain && remain < 5) climb_state = CLIMB;
+          break;
+        case CLIMB:
+          tau_pitch = PIDcontrol(mpu.getPitch(),pitchZero,10,&pitch_before,-1,0,0);
+          if(echo() > climb_start_alt) climb_state = SECOND_TURN;
+          break;
+        case SECOND_TURN:
+          tau_pitch = PIDcontrol(mpu.getPitch(),pitchZero,0,&pitch_before,-1,0,0);
+          break;
+        default:
+          break;
+        }
+      }
       rudder.write(tau_roll+90);
       elevator.write(tau_pitch+90);
     }
-    else{
+    else{ //手動操縦モード
       digitalWrite(PIN_LED1,LOW);
+      eight_state = BEFORE_START;
       if(index > 10){
           int rtemp = 180-((float)(((dat[6] & 0x0F) << 7)+((dat[5] & 0xFE) >> 1)-192)/1600*180);
           rudder.write((int)(rtemp + rtemp1 + rtemp2)/3);
@@ -106,6 +159,19 @@ void loop() {
         }
       }
   }
+}
+
+float echo(){
+  digitalWrite(11, LOW);
+  delayMicroseconds(2);
+  digitalWrite(11, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(11, LOW);
+
+  dur = pulseIn(7, HIGH);
+  dist = (dur*0.034)/2.0;
+
+  return dist;
 }
 
 // argument "current" should be the raw data from the sensor (therefore biased by the value set at calibration) and argument "target" should not be biased (therefore the argument should be 0 when completely horizontal). The return value is also biased.
